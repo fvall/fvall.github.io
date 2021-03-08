e44af0abb9884f08f6a774b207ea48d5d59ced33
Felipe Valladao <felipe.valladao@gmail.com>
2021-03-08 23:40:53 +0000

    pro mobile version

--- components/prof/about.js
+++ components/prof/about.js
@@ -4,12 +4,17 @@ export default function About() {
   return (
     <div className="prof-section prof-about">
       <SectionTitle title={"About me"} />
-      <p>
+      <p className="description">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil accusamus
         quo quia beatae, perferendis illum aperiam repellendus cupiditate
         ducimus ut vitae eveniet officiis odio dicta. Unde laboriosam aliquam
         velit facilis?
       </p>
+      <ul>
+        <li>a</li>
+        <li>b</li>
+        <li>c</li>
+      </ul>
     </div>
   );
 }
